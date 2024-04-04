4e1e2e9626abe370d450761559216e6518e843dc
Ivan Vasilyev <eyewan@Ivans-MacBook-Air.local>
2024-04-04 19:56:19 +0300

    add write to json file

--- cmd/gocert/main.go
+++ cmd/gocert/main.go
@@ -5,25 +5,55 @@ import (
 	"flag"
 	"fmt"
 	"gocert/internal/cert"
+	"gocert/internal/utils"
+	"time"
 )
 
 func main() {
 
 	domainPtr := flag.String("d", "", "Domain name")
+	filePtr := flag.String("f", "", "Path to file")
+	helpPtr := flag.Bool("h", false, "Help")
 
 	flag.Parse()
 
-	if *domainPtr == "" {
-		fmt.Println("Please provide a domain:\ngo run main.go -d 'domain'")
+	if *helpPtr {
+		flag.Usage()
 		return
 	}
 
-	parsedCert, err := cert.Parse(*domainPtr)
-	if err != nil {
-		panic(err)
+	if *domainPtr != "" && *filePtr != "" {
+		fmt.Println("You can't provide both domain and path")
+		return
 	}
 
-	jsonData, _ := json.MarshalIndent(parsedCert, "", "  ")
-	fmt.Println(string(jsonData))
+	if *domainPtr == "" && *filePtr == "" {
+		fmt.Println("Please provide a domain or a file:")
+		fmt.Println("./gocert -d 'domain'")
+		fmt.Println("./gocert -f 'file'")
+		return
+	}
+
+	start := time.Now()
+
+	if *domainPtr != "" {
+		_, err := cert.Parse(*domainPtr)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		certificatesMetadata, err := cert.ParseFromFile(*filePtr)
+		if err != nil {
+			fmt.Printf("Can't read data from file: %v", err)
+		}
+		jsonData, _ := json.MarshalIndent(certificatesMetadata, "", "  ")
+		err = utils.WriteToJSON(jsonData, "test.json")
+		if err != nil {
+			fmt.Printf("Can't write to json file: %v\n", err)
+		}
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Execution time: %s\n", elapsed)
 
 }
